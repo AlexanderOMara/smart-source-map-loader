58f73dfabe98e1c63eabd024ae7b5dd8f1d956a9
Alexander O'Mara <me@alexomara.com>
2020-10-13 22:53:49 -0400

    Better bad URI example

--- spec/fixtures/datauri-bad/min.js
+++ spec/fixtures/datauri-bad/min.js
@@ -1,3 +1,3 @@
  ONE.foo=function(a){return baz(a);};
  TWO.inc=function(a){return a+1;};
-//# sourceMappingURL=data:application/source-map;charset=utf-8;base64,_BAD_DATA_URI_
+//# sourceMappingURL=data:application/source-map;charset=utf-8;base64
