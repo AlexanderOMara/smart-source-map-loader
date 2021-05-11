a7508a034c35308111fe079b791f8ae0accf81a0
Alexander O'Mara <me@alexomara.com>
2021-05-10 21:01:19 -0400

    Gulp watch instead of nodemon, faster

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -185,6 +185,16 @@ gulp.task('test', gulp.parallel([
 	'test:node'
 ]));
 
+// watch
+
+gulp.task('watch', () => {
+	gulp.watch([
+		'src/**/*'
+	], gulp.series([
+		'all'
+	]));
+});
+
 // all
 
 gulp.task('all', gulp.series([
@@ -197,10 +207,8 @@ gulp.task('all', gulp.series([
 // watched
 
 gulp.task('watched', gulp.series([
-	'clean',
-	'lintw',
-	'build',
-	'test'
+	'all',
+	'watch'
 ]));
 
 // prepack
